c96217c82b64c820b8bc0080f8a524f3646a084a
Valentin Saugnier <valentin.s.10@gmail.com>
2016-12-09 14:25:42 +0100

    Search OK V2

--- app/src/main/java/lpsmin/randsode/MainActivity.java
+++ app/src/main/java/lpsmin/randsode/MainActivity.java
@@ -1,22 +1,22 @@
 package lpsmin.randsode;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
+import android.view.Menu;
+import android.support.v7.widget.SearchView;
 
 import java.util.ArrayList;
 
 import info.movito.themoviedbapi.model.tv.TvSeries;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private RecyclerViewAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +26,43 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        AutoCompleteTextView search_field = (AutoCompleteTextView) findViewById(R.id.main_search_field2);
 
         ArrayList<TvSeries> series = new ArrayList<>();
 
-        final RecyclerView list = (RecyclerView) findViewById(R.id.main_list);
-        final RecyclerViewAdapter listAdapter = new RecyclerViewAdapter(this, series);
+       RecyclerView list = (RecyclerView) findViewById(R.id.main_list);
+        this.listAdapter = new RecyclerViewAdapter(this, series);
         list.setAdapter(listAdapter);
         list.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-        search_field.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    if (textView.getText().length() > 0) {
-                        SearchTask task = new SearchTask(textView.getText().toString(), listAdapter);
-                        task.execute();
-                        InputMethodManager imm = (InputMethodManager) textView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                        imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
-                    }
-                }
-                return false;
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        if (query.length() > 0) {
+            SearchTask task = new SearchTask(query, this.listAdapter);
+            task.execute();
+//            InputMethodManager imm = (InputMethodManager) textView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+//            imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
